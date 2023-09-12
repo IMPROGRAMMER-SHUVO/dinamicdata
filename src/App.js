aae577634213f7740146be2f3f47d1e8edec82cb
IMPROGRAMMER-SHUVO <shuvodey1102@gmail.com>
2023-09-12 10:29:17 +0600

    this is prpsdata

--- src/App.js
+++ src/App.js
@@ -2,24 +2,39 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+const products=[
+  {name:' laptop',price:'53000'},
+  {name:' iphone',price:'34000'},
+  {name:' Tab',price:'5000'},
+  {name:' watch',price:'3000'},
+]
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      {
+        products.map(product=><div className='allnewadd'><li>{product.name}={product.price}</li></div>)
+      },
+      {
+        products.map(product=> <Productt name={product.name} price={product.price}></Productt>)
+      }
+
+
+ {/* <Productt name='laptop'price='55556'></Productt> */}
+ 
+ 
+ 
+ 
+ </div>
   );
+}
+function Productt(props){
+  return(
+    <div className='divs'>
+      <h1>name: {props.name}</h1>
+      <p> price:{props.price}</p>
+    </div>
+  )
 }
 
 export default App;
